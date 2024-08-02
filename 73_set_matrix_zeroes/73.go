7cb0f559d4bc6f3ae07c05bf8279eb9f941ecaa7
Juan Benavente <jjbenavaz@gmail.com>
2024-08-02 00:11:51 -0600

    added 73 inneficient solution in go

---
+++ 73_set_matrix_zeroes/73.go
@@ -0,0 +1,41 @@
+// ineficient solution
+
+func setZeroes(matrix [][]int) {
+	output := make([][]int, len(matrix))
+	deepCopy(matrix, output)
+	for i, row := range matrix {
+		// fmt.Println(val)
+		for j, val := range row {
+			// fmt.Println(val)
+			if val == 0 {
+				// set row to zeroes
+				for a, _ := range output[i] {
+					output[i][a] = 0
+				}
+
+				// set column to zeroes
+				for _, row := range output {
+					for b, _ := range row {
+						if b == j {
+							row[b] = 0
+						}
+					}
+				}
+			}
+		}
+	}
+	// fmt.Println(matrix)
+	// fmt.Println(output)
+	deepCopy(output, matrix)
+}
+
+// create an output deepcopy
+func deepCopy(matrix [][]int, dst [][]int) {
+	// output := make([][]int, len(matrix))
+	for i := range matrix {
+		inner := make([]int, len(matrix[i]))
+		copy(inner, matrix[i])
+		dst[i] = inner
+	}
+
+}
